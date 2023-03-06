f7e36bf37d1bcef61a5923b1ff1c38aa60ccfc5b
Euan Scott-Watson <es1519@ic.ac.uk>
2023-03-06 15:55:10 +0000

    Fixed plotting scores

--- roberta_model/model_eval/plot_model_scores.py
+++ roberta_model/model_eval/plot_model_scores.py
@@ -1,6 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 import argparse
+from collections import OrderedDict
 
 def plot(json_path, save_path):
 
@@ -8,11 +9,18 @@ def plot(json_path, save_path):
     with open(json_path, 'r') as f:
         data = json.load(f)
 
+    formatted_data = {}
+    for epoch_str, scores in data.items():
+        epoch_string = epoch_str.split("-step")[0].split("=")[1]
+        formatted_data[int(epoch_string)] = scores
+
+    ordered_data = OrderedDict(sorted(formatted_data.items()))
+
     # Extract the scores for each metric
-    acc_scores = [epoch_data['accuracy'] for epoch_data in data.values()]
-    prec_scores = [epoch_data['precision'] for epoch_data in data.values()]
-    rec_scores = [epoch_data['recall'] for epoch_data in data.values()]
-    f1_scores = [epoch_data['f1_score'] for epoch_data in data.values()]
+    acc_scores = [epoch_data['accuracy'] for epoch_data in ordered_data.values()]
+    prec_scores = [epoch_data['precision'] for epoch_data in ordered_data.values()]
+    rec_scores = [epoch_data['recall'] for epoch_data in ordered_data.values()]
+    f1_scores = [epoch_data['f1_score'] for epoch_data in ordered_data.values()]
 
     # Plot the scores over time
     epochs = range(len(data))
